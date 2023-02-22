3cad5c9630044d5e0443ac90ad4fd7bad06dfb21
Ash Bridges <ashbridges@virginmedia.com>
2023-02-22 22:46:44 +0000

    added image animations

--- components/About.js
+++ components/About.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Image from 'next/image'
 
 import { useInView } from 'react-intersection-observer';
@@ -11,18 +11,35 @@ import AnimatedText from "./AnimatedText";
 
 function About({ offsetY }) {
 
-    const placeholderText = [
-        { type: "heading", text: "A little about me." }
-    ];
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let interval = null;
 
-    const container = {
-        visible: {
-            transition: {
-                staggerChildren: 0.03
-            }
-        }
-    };
-    
+    useEffect(() => {
+        document.querySelector('#aboutTitle').onmouseover = e => {
+            let iteration = 0;
+            clearInterval(interval);
+
+            interval = setInterval(() => {
+                e.target.innerText = e.target.innerText
+                .split("")
+                .map((letter, index) => {
+                    if(index < iteration) {
+                    return e.target.dataset.value[index];
+                    }
+                
+                    return letters[Math.floor(Math.random() * 26)]
+                })
+                .join("");
+                
+                if(iteration >= e.target.dataset.value.length){ 
+                clearInterval(interval);
+                }
+                
+                iteration += 1 / 3;
+            }, 10);
+        }   
+    })
+
     const { ref, inView } = useInView({
         triggerOnce: true,
         threshold: 0.475
@@ -35,10 +52,9 @@ function About({ offsetY }) {
                 <motion.div className={AboutStyles.aboutHeader}
                 initial="hidden"
                 animate={inView ? "visible" : "hidden"}
-                variants={container}
                 >
                 <span>.01</span>
-                <h2 className={AboutStyles.aboutTitle}>
+                <h2 data-value="A little about me." id="aboutTitle" className={AboutStyles.aboutTitle}>
                     A little about me.
                     {/* {placeholderText.map((item, index) => {
                         return <AnimatedText {...item} key={index} />;
@@ -46,15 +62,20 @@ function About({ offsetY }) {
                 </h2>
                 </motion.div>
                 <div className={AboutStyles.aboutText}>
-                    <p>
+                    <motion.p 
+                    initial={{ opacity: 0}}
+                    animate={{ opacity: 1}} 
+                    transition={{ duration: 1 }}>
                         Hey there, I'm Ash, a <span>web developer</span> from the UK. I have a background in visual communications, and have since turned to web development, combining my creative experience with my more technical interests.<br /><br />Always eager to learn new skills, I enjoy spending my time learning <span>new ways to bring designs to life</span> and helping people make their mark on the web. Feel free to browse over my work, and thanks for stopping by!
-                    </p>
-                    <div className={AboutStyles.aboutImage}  >
-                        <Image
-                            fill
-                            src={me}
-                            alt="Ash Bridges"
-                        />
+                    </motion.p>
+                    <div className={AboutStyles.box}>
+                        <div className={AboutStyles.aboutImage}>
+                            <Image
+                                fill
+                                src={me}
+                                alt="Ash Bridges"
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
