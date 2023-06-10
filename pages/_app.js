205e2d12918661b1456653e9e121a80460188c0a
Ash Bridges <ashbridges@virginmedia.com>
2023-06-10 09:32:26 +0100

    lenis scroll

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import '../styles/globals.scss'
-import Background from '../components/Background'
+import "../styles/globals.scss";
+import Background from "../components/Background";
+import { Lenis as ReactLenis } from "@studio-freight/react-lenis";
 
 export default function App({ Component, pageProps }) {
-
   return (
     <>
-      <Background />
+      <ReactLenis root >
+        <Background />
         <Component {...pageProps} />
+      </ReactLenis>
     </>
-  )
+  );
 }
