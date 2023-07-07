4c942c5f57d9fd44df9ae7a394a91e0f4fe2dc7a
Ash Bridges <ashbridges@virginmedia.com>
2023-07-07 12:42:36 +0100

    rearranged and updated marquet

--- components/Projects/ProjectsContent.js
+++ components/Projects/ProjectsContent.js
@@ -1,25 +1,5 @@
 export const ProjectsContent  = [
     {
-        image: '/marquet1.svg',
-        gallery: [
-            '/marquet1.svg',
-            '/marquet2.svg',
-            '/marquet4.svg',
-            '/marquet3.svg',
-        ],
-        type: 'Featured Project',
-        title: 'Marquet',
-        desc: 'Marquet is a responsive ecommerce multipage website that features full cart functionality. Built with HTML, CSS and JS.',
-        longerDesc: 'Built with HTML, and utilising SASS and pure JS, Marquet is an ecommerce site that features full cart functionality,and uses session and local storage to simulate a backend. ',
-        languages: [
-            'HTML',
-            'CSS',
-            'Javascript'
-        ],
-        siteLink: 'https://marquet-ecommerce.web.app/',
-        gitLink: 'https://github.com/HighRoller94/marquet-ecommerce'
-    },
-    {
         image: '/shack1.svg',
         gallery: [
             '/shack1.svg',
@@ -41,6 +21,27 @@ export const ProjectsContent  = [
         gitLink: 'https://github.com/HighRoller94/redux-pizza-app'
     },
     {
+        image: '/marquet1.svg',
+        gallery: [
+            '/marquet1.svg',
+            '/marquet2.svg',
+            '/marquet4.svg',
+            '/marquet3.svg',
+        ],
+        type: 'Featured Project',
+        title: 'Marquet',
+        desc: 'Marquet is a scalable ecommerce site that features full cart functionality and third party user authentication. Built with Next.js and Mongodb.',
+        longerDesc: 'Built with HTML, and utilising SASS and pure JS, Marquet is an ecommerce site that features full cart functionality, and uses session and local storage to simulate a backend. ',
+        languages: [
+            'Next.js',
+            'Mongodb',
+            'Redux'
+        ],
+        siteLink: 'https://marquet-nextjs.vercel.app/',
+        gitLink: 'https://github.com/HighRoller94/marquet-ecommerce'
+    },
+    
+    {
         image: '/pyre1.svg',
         gallery: [
             '/pyre2.svg',
@@ -51,12 +52,12 @@ export const ProjectsContent  = [
         ],
         type: 'Featured Project',
         title: 'Pyre',
-        desc: 'Built with React, Pyre uses the Spotify API to allows users to login, listen and manage their media. Users can add playlists and tracks to favourites, create new playlists entirely and follow artists/other Spotify users.',
+        desc: 'Pyre uses the Spotify API to allows users to login, listen and manage their media. Users can add playlists and tracks to favourites, create new playlists and follow artists/other Spotify users.',
         longerDesc: 'Music player with a simple dashboard layout. Built with React, Pyre uses the Spotify API to allows users to login, listen and manage their media. Users can add playlists and tracks to favourites, create new playlists entirely as well as follow artists/other Spotify users. Pyre utilises React Context and an express server for authentication purposes on the backend. Due to the limitations of the Spotify API, only premium users can use Pyre to listen to their favourite media.',
         languages: [
-            'React',
-            'Express',
-            'Spotify'
+            'Next.js',
+            'Zustand',
+            'Tailwind'
         ],
         siteLink: 'https://pyre-2e47e.web.app/',
         gitLink: 'https://github.com/HighRoller94/pyre'
@@ -82,6 +83,32 @@ export const ProjectsContent  = [
     // },
 ]
   
+export const NotableProjects  = [
+    {
+        image: '/marquet1.svg',
+        type: 'Commissioned Project',
+        title: 'Three Sheets to the Wind',
+        desc: 'Built for the idyllic Austrian-Anglo micropub located in the heart of Rochester. This site features a fully customisable backend complete with events page, menu and booking system.',
+        languages: [
+            'Next.js',
+            'Sanity'
+        ],
+        siteLink: 'https://three-sheets.vercel.app',
+    },
+    {
+        image: '/marquet1.svg',
+        type: 'Commissioned Project',
+        title: 'Jamie Taplin Music',
+        desc: 'Commissioned project for local music teaching business based in Rochester. This webapp features a booking system using Sanity as a backend connected to its own personal business calendar.',
+        languages: [
+            'Next.js',
+            'Sanity'
+        ],
+        siteLink: 'https://static-template-site.vercel.app/',
+    },
+]
+
 export default {
     ProjectsContent,
+    NotableProjects
 }
