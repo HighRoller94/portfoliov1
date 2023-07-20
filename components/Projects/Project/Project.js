fd6ff2e2b6f47948525ff2c8eb267d7f9dc7c5e7
Ash Bridges <ashbridgescodes@gmail.com>
2023-07-20 12:03:34 +0100

    changed projects, got rid of modals. added cursor

--- components/Projects/Project/Project.js
+++ components/Projects/Project/Project.js
@@ -1,73 +1,76 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import ProjectStyles from '../../../styles/components/Project.module.scss';
-import { useInView } from 'react-intersection-observer';
+import ProjectStyles from "../../../styles/components/Project.module.scss";
+import { useInView } from "react-intersection-observer";
 
-import { ImGithub } from 'react-icons/im';
-import { BiWorld } from 'react-icons/bi';
+import { ImGithub } from "react-icons/im";
+import { BiWorld } from "react-icons/bi";
 
-function Project({ data, setModalData, handleModal, title, type, desc, image, gitLink, siteLink, languages, offsetY }) {
+function Project({
+  data,
+  setModalData,
+  handleModal,
+  title,
+  type,
+  desc,
+  image,
+  gitLink,
+  siteLink,
+  languages,
+  offsetY,
+}) {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.2,
+  });
 
-    const { ref, inView } = useInView({
-        triggerOnce: true,
-        threshold: 0.2
-    });
-
-    return (
-        <>
-            <div ref={ref} className={inView ? `${ProjectStyles.project} showProject` : `${ProjectStyles.project} hideProject`}>
-                <div className={`${ProjectStyles.projectImage} projectImage`} onClick={() => {
-                    setModalData(data)
-                    handleModal()
-                }}>
-                    <Image
-                        src={`/images/${image}`}
-                        alt="Project Image"
-                        fill
-                    />
-                    <div className={ProjectStyles.projectImageText}>
-                        <h1>Learn More</h1>
-                        <div className={ProjectStyles.innerText}>
-                            <p>Read more about this project</p>
-                            <span className={ProjectStyles.shaft}>
-                                <div className={ProjectStyles.head}>
-                                    <span></span>
-                                    <span></span>
-                                </div>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <div className={ProjectStyles.projectText}>
-                    <h4>{type}</h4>
-                    <h1>{title}</h1>
-                    <p>{desc}</p>
-                    <div className={ProjectStyles.projectLanguages}>
-                        {languages.map((language, index) => (
-                            <span key={index}>{language}</span>
-                        ))}
-                    </div>
-                    <div className={ProjectStyles.btns}>
-                        <a className="link" href={siteLink} target="_blank" rel="noreferrer">
-                            <button className={`${ProjectStyles.siteBtn} focus`}>
-                                <BiWorld className={ProjectStyles.icon} />
-                                Live Site
-                            </button>
-                        </a>
-                        <a className="link" href={gitLink} target="_blank" rel="noreferrer">
-                            <button className={`${ProjectStyles.gitBtn} focus`}>
-                                <ImGithub className={ProjectStyles.icon} />
-                                View Code
-                            </button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-
-        </>
-
-
-    )
+  return (
+    <>
+      <div
+        ref={ref}
+        className={
+          inView
+            ? `${ProjectStyles.project} showProject`
+            : `${ProjectStyles.project} hideProject`
+        }
+      >
+        <a className="imageLink" href={siteLink} target="_blank" rel="noreferrer">
+          <div className={`${ProjectStyles.projectImage} projectImage`}>
+            <Image src={`/images/${image}`} alt="Project Image" fill />
+          </div>
+        </a>
+        <div className={ProjectStyles.projectText}>
+          <h4>{type}</h4>
+          <h1>{title}</h1>
+          <p>{desc}</p>
+          <div className={ProjectStyles.projectLanguages}>
+            {languages.map((language, index) => (
+              <span key={index}>{language}</span>
+            ))}
+          </div>
+          <div className={ProjectStyles.btns}>
+            <a
+              className="link"
+              href={siteLink}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <button className={`${ProjectStyles.siteBtn} focus`}>
+                <BiWorld className={ProjectStyles.icon} />
+                Live Site
+              </button>
+            </a>
+            <a className="link" href={gitLink} target="_blank" rel="noreferrer">
+              <button className={`${ProjectStyles.gitBtn} focus`}>
+                <ImGithub className={ProjectStyles.icon} />
+                View Code
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Project
+export default Project;
